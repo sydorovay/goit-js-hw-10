c0679bfe10cfe6f8deb8883105edacfb288ed6c2
Artem Sydorov <sydorovay@gmail.com>
2023-04-12 23:23:22 +0200

    start task
    import fetchCountries, showNotification,debounce
    links of elements
    create input.addEventListener
    create function clearCountryMarkup
    create function onInputSearch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import { showNotification } from './js/notiflixMessage';
+import debounce from 'lodash.debounce';
+// import showNotification from 'notiflix';
 
+// значення затримки DEBOUNCE
 const DEBOUNCE_DELAY = 300;
+
+// посилання на елементи
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+// слухач подій для поля input з затримкою DEBOUNCE
+input.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+
+function clearCountryMarkup() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+}
+
+function onInputSearch(event) {
+	event.preventDefault();
+	const searchCountryName = event.target.value.trim();
+	if (!searchCountryName) {
+		clearCountryMarkup();
+		return;
+	}
+}
+
